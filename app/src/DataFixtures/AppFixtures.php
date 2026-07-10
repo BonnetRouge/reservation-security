312ae541706f966147a435b990e6d16b515f9b35
BonnetRouge <david.bie.sop.emm1@gmail.com>
2026-07-10 11:23:47 +0200

    Configure JWT login (username_path/password_path) + routes.yaml

---
+++ app/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('user@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail('admin@test.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'password123'));
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
