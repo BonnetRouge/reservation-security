8a60aac204c9dccde4965c43fdea38c3fc256d47
BonnetRouge <david.bie.sop.emm1@gmail.com>
2026-07-18 22:32:55 +0200

    Fix SQL Injection: use parameterized query in SearchController

--- app/src/Controller/SearchController.php
+++ app/src/Controller/SearchController.php
@@ -7,18 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SearchController extends AbstractController
 {
     #[Route('/api/reservations/search', name: 'api_reservations_search', methods: ['GET'], priority: 10)]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function search(Request $request, Connection $connection): JsonResponse
     {
         $query = $request->query->get('q', '');
 
-        // VULNÉRABLE : concaténation directe de l'entrée utilisateur dans la requête SQL
-        $sql = "SELECT * FROM reservation WHERE resource_name LIKE '%" . $query . "%'";
+        $sql = 'SELECT * FROM reservation WHERE resource_name LIKE :search';
 
-        $results = $connection->fetchAllAssociative($sql);
+        $results = $connection->fetchAllAssociative($sql, [
+            'search' => '%' . $query . '%',
+        ]);
 
         return $this->json($results);
     }
